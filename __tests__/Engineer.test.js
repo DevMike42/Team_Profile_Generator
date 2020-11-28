466000e52dca117bed1ac353e721309635b9b684
Mike Rivera <michaelstephenrivera@gmail.com>
2020-11-28 13:30:12 -0600

    Created tests for Engineer class constructor

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,3 +4,21 @@ test('Can instantiate instance of an Employee', () => {
   const e = new Engineer();
   expect(typeof (e)).toBe('object');
 });
+
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "MortyC137";
+  const e = new Engineer("Morty Smith", 137, "ohgeez@examples.com", testValue);
+  expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Morty Smith", 137, "ohgeez@examples.com", "MortyC137");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "MortyC137";
+  const e = new Engineer("Morty Smith", 137, "ohgeez@examples.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
