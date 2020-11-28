fdde8541374d25060cd1eb020c7b24cc8cacf96a
Mike Rivera <michaelstephenrivera@gmail.com>
2020-11-28 13:27:19 -0600

    Created tests for Manager class constructor

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,3 +4,21 @@ test('Can instantiate instance of an Employee', () => {
   const e = new Manager();
   expect(typeof (e)).toBe('object');
 });
+
+test('Can set office number via constructor argument', () => {
+  const testValue = 100;
+  const e = new Manager('Rick Sanchez', 137, 'burp@examples.com', testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
+
+test('getRole() shoudl return \"Manager\"', () => {
+  const testValue = 'Manager';
+  const e = new Manager('Rick Sanchez', 137, 'burp@examples.com', 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test('Can get office number vie getOffice()', () => {
+  const testValue = 100;
+  const e = new Manager('Rick Sanchez', 137, 'burp@examples.com', testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
