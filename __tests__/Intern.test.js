fbdbcc36f6a3e80d5845a033a60f677bc17fe7d4
Mike Rivera <michaelstephenrivera@gmail.com>
2020-11-28 13:32:32 -0600

    Created tests for Intern class constructor

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -4,3 +4,21 @@ test('Can instantiate instance of an Employee', () => {
   const e = new Intern();
   expect(typeof (e)).toBe('object');
 });
+
+test("Can set school via constructor", () => {
+  const testValue = "Herpson High";
+  const e = new Intern("Morty Smith", 137, "ohgeez@examples.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Morty Smith", 137, "ohgeez@examples.com", "Herpson High");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "Herpson High";
+  const e = new Intern("Morty Smith", 137, "ohgeez@examples.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
