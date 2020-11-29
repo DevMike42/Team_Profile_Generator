9c6f2903aa9ee73a50f9e41fc152b1536cedf049
Mike Rivera <michaelstephenrivera@gmail.com>
2020-11-29 13:51:26 -0600

    Created inquirer prompt for gathering user input to create a Manager object

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('Working');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+
+const teamMembers = [];
+
+const init = () => {
+  createManager();
+};
+
+const createManager = () => {
+  console.log(`Let's build your team!`);
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'managerName',
+      message: `What is your team Manager's name?`
+    },
+    {
+      type: 'input',
+      name: 'managerId',
+      message: `What is your team Manager's ID?`
+    },
+    {
+      type: 'input',
+      name: 'managerEmail',
+      message: `What is your team Manager's email address?`
+    },
+    {
+      type: 'input',
+      name: 'managerOfficeNum',
+      message: `What is your team Manager's office number?`
+    }
+  ])
+    .then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
+      teamMembers.push(manager);
+      console.log(teamMembers);
+
+    })
+    .catch(err => {
+      if (err) {
+        throw err;
+      };
+    });
+
+};
+
+init();
