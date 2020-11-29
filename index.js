85ad7b8fe5f3bd32e797472112e6d40f01f492fd
Mike Rivera <michaelstephenrivera@gmail.com>
2020-11-29 14:03:06 -0600

    Added validation on user input for manager creation

--- index.js
+++ index.js
@@ -13,22 +13,55 @@ const createManager = () => {
     {
       type: 'input',
       name: 'managerName',
-      message: `What is your team Manager's name?`
+      message: `What is your team Manager's name?`,
+      validate: answer => {
+        if (answer !== '') {
+          return true;
+        }
+        return 'Please enter at least one character.'
+      }
     },
     {
       type: 'input',
       name: 'managerId',
-      message: `What is your team Manager's ID?`
+      message: `What is your team Manager's ID?`,
+      validate: answer => {
+        const pass = answer.match(
+          /^[1-9]\d*$/
+        );
+        if (pass) {
+          return true;
+        }
+        return 'Please enter a positive number greater than zero.'
+      }
     },
     {
       type: 'input',
       name: 'managerEmail',
-      message: `What is your team Manager's email address?`
+      message: `What is your team Manager's email address?`,
+      validate: answer => {
+        const pass = answer.match(
+          /\S+@\S+\.\S+/
+        );
+        if (pass) {
+          return true;
+        }
+        return 'Please enter a valid email address.'
+      }
     },
     {
       type: 'input',
       name: 'managerOfficeNum',
-      message: `What is your team Manager's office number?`
+      message: `What is your team Manager's office number?`,
+      validate: answer => {
+        const pass = answer.match(
+          /^[1-9]\d*$/
+        );
+        if (pass) {
+          return true;
+        }
+        return 'Please enter a positive number greater than zero.'
+      }
     }
   ])
     .then(answers => {
